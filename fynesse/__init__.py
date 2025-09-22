72a38e76dcee4c8918dbf346456074f897d041cc
miketech-traces <muthomimike950@gmail.com>
2025-09-22 22:32:28 +0300

    Update __init__.py

--- fynesse/__init__.py
+++ fynesse/__init__.py
@@ -1,15 +1,1 @@
-from . import access
-from . import assess
-# fynesse/__init__.py
-from .access import (
-    load_maize_data,
-    load_population_data,
-    load_agricultural_production,
-)
-
-__all__ = [
-    "load_maize_data",
-    "load_population_data",
-    "load_agricultural_production",
-]
 
